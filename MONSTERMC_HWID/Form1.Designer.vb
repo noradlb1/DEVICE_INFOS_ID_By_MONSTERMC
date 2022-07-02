668100e2c4f9bf7bb831f9389c6d4ce47ebd84d2
MONSTERMC <zcyfkhuru.bz@mail.ru>
2022-07-02 14:02:03 +0200

    DEVICE_INFOS_ID_By_MONSTERMC
    DEVICE_INFOS_ID_By_MONSTERMC

---
+++ MONSTERMC_HWID/Form1.Designer.vb
@@ -0,0 +1,339 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblNetwork = New System.Windows.Forms.Label()
+        Me.FormSkin1 = New MONSTERMC_HWID.FormSkin()
+        Me.FlatLabel8 = New MONSTERMC_HWID.FlatLabel()
+        Me.FlatLabel7 = New MONSTERMC_HWID.FlatLabel()
+        Me.FlatLabel6 = New MONSTERMC_HWID.FlatLabel()
+        Me.FlatLabel5 = New MONSTERMC_HWID.FlatLabel()
+        Me.FlatLabel4 = New MONSTERMC_HWID.FlatLabel()
+        Me.FlatLabel3 = New MONSTERMC_HWID.FlatLabel()
+        Me.FlatTextBox6 = New MONSTERMC_HWID.FlatTextBox()
+        Me.FlatTextBox5 = New MONSTERMC_HWID.FlatTextBox()
+        Me.FlatTextBox4 = New MONSTERMC_HWID.FlatTextBox()
+        Me.FlatTextBox3 = New MONSTERMC_HWID.FlatTextBox()
+        Me.FlatTextBox2 = New MONSTERMC_HWID.FlatTextBox()
+        Me.FlatLabel2 = New MONSTERMC_HWID.FlatLabel()
+        Me.FlatLabel1 = New MONSTERMC_HWID.FlatLabel()
+        Me.FlatTextBox1 = New MONSTERMC_HWID.FlatTextBox()
+        Me.FlatMini1 = New MONSTERMC_HWID.FlatMini()
+        Me.FlatClose1 = New MONSTERMC_HWID.FlatClose()
+        Me.FormSkin1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblNetwork
+        '
+        Me.lblNetwork.AutoSize = True
+        Me.lblNetwork.Location = New System.Drawing.Point(12, 195)
+        Me.lblNetwork.Name = "lblNetwork"
+        Me.lblNetwork.Size = New System.Drawing.Size(0, 13)
+        Me.lblNetwork.TabIndex = 3
+        '
+        'FormSkin1
+        '
+        Me.FormSkin1.BackColor = System.Drawing.Color.White
+        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.FormSkin1.Controls.Add(Me.FlatLabel8)
+        Me.FormSkin1.Controls.Add(Me.FlatLabel7)
+        Me.FormSkin1.Controls.Add(Me.FlatLabel6)
+        Me.FormSkin1.Controls.Add(Me.FlatLabel5)
+        Me.FormSkin1.Controls.Add(Me.FlatLabel4)
+        Me.FormSkin1.Controls.Add(Me.FlatLabel3)
+        Me.FormSkin1.Controls.Add(Me.FlatTextBox6)
+        Me.FormSkin1.Controls.Add(Me.FlatTextBox5)
+        Me.FormSkin1.Controls.Add(Me.FlatTextBox4)
+        Me.FormSkin1.Controls.Add(Me.FlatTextBox3)
+        Me.FormSkin1.Controls.Add(Me.FlatTextBox2)
+        Me.FormSkin1.Controls.Add(Me.FlatLabel2)
+        Me.FormSkin1.Controls.Add(Me.FlatLabel1)
+        Me.FormSkin1.Controls.Add(Me.FlatTextBox1)
+        Me.FormSkin1.Controls.Add(Me.FlatMini1)
+        Me.FormSkin1.Controls.Add(Me.FlatClose1)
+        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkin1.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(47, Byte), Integer), CType(CType(54, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.FormSkin1.HeaderMaximize = False
+        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkin1.Name = "FormSkin1"
+        Me.FormSkin1.Size = New System.Drawing.Size(295, 334)
+        Me.FormSkin1.TabIndex = 5
+        Me.FormSkin1.Text = "MONSTERMC DEVICE INFOS ID"
+        '
+        'FlatLabel8
+        '
+        Me.FlatLabel8.AutoSize = True
+        Me.FlatLabel8.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel8.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel8.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel8.Location = New System.Drawing.Point(13, 267)
+        Me.FlatLabel8.Name = "FlatLabel8"
+        Me.FlatLabel8.Size = New System.Drawing.Size(29, 13)
+        Me.FlatLabel8.TabIndex = 20
+        Me.FlatLabel8.Text = "GPU"
+        '
+        'FlatLabel7
+        '
+        Me.FlatLabel7.AutoSize = True
+        Me.FlatLabel7.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel7.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel7.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel7.Location = New System.Drawing.Point(12, 222)
+        Me.FlatLabel7.Name = "FlatLabel7"
+        Me.FlatLabel7.Size = New System.Drawing.Size(31, 13)
+        Me.FlatLabel7.TabIndex = 19
+        Me.FlatLabel7.Text = "MAC"
+        '
+        'FlatLabel6
+        '
+        Me.FlatLabel6.AutoSize = True
+        Me.FlatLabel6.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel6.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel6.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel6.Location = New System.Drawing.Point(12, 176)
+        Me.FlatLabel6.Name = "FlatLabel6"
+        Me.FlatLabel6.Size = New System.Drawing.Size(23, 13)
+        Me.FlatLabel6.TabIndex = 18
+        Me.FlatLabel6.Text = "MB"
+        '
+        'FlatLabel5
+        '
+        Me.FlatLabel5.AutoSize = True
+        Me.FlatLabel5.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel5.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel5.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel5.Location = New System.Drawing.Point(12, 135)
+        Me.FlatLabel5.Name = "FlatLabel5"
+        Me.FlatLabel5.Size = New System.Drawing.Size(28, 13)
+        Me.FlatLabel5.TabIndex = 17
+        Me.FlatLabel5.Text = "CPU"
+        '
+        'FlatLabel4
+        '
+        Me.FlatLabel4.AutoSize = True
+        Me.FlatLabel4.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel4.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel4.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel4.Location = New System.Drawing.Point(13, 90)
+        Me.FlatLabel4.Name = "FlatLabel4"
+        Me.FlatLabel4.Size = New System.Drawing.Size(31, 13)
+        Me.FlatLabel4.TabIndex = 16
+        Me.FlatLabel4.Text = "HDD"
+        '
+        'FlatLabel3
+        '
+        Me.FlatLabel3.AutoSize = True
+        Me.FlatLabel3.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel3.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel3.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel3.Location = New System.Drawing.Point(13, 43)
+        Me.FlatLabel3.Name = "FlatLabel3"
+        Me.FlatLabel3.Size = New System.Drawing.Size(37, 13)
+        Me.FlatLabel3.TabIndex = 15
+        Me.FlatLabel3.Text = "HWID"
+        '
+        'FlatTextBox6
+        '
+        Me.FlatTextBox6.BackColor = System.Drawing.Color.Transparent
+        Me.FlatTextBox6.Location = New System.Drawing.Point(11, 283)
+        Me.FlatTextBox6.MaxLength = 32767
+        Me.FlatTextBox6.Multiline = False
+        Me.FlatTextBox6.Name = "FlatTextBox6"
+        Me.FlatTextBox6.ReadOnly = False
+        Me.FlatTextBox6.Size = New System.Drawing.Size(270, 29)
+        Me.FlatTextBox6.TabIndex = 14
+        Me.FlatTextBox6.Text = "Generating GPU..."
+        Me.FlatTextBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.FlatTextBox6.TextColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.FlatTextBox6.UseSystemPasswordChar = False
+        '
+        'FlatTextBox5
+        '
+        Me.FlatTextBox5.BackColor = System.Drawing.Color.Transparent
+        Me.FlatTextBox5.Location = New System.Drawing.Point(11, 238)
+        Me.FlatTextBox5.MaxLength = 32767
+        Me.FlatTextBox5.Multiline = False
+        Me.FlatTextBox5.Name = "FlatTextBox5"
+        Me.FlatTextBox5.ReadOnly = False
+        Me.FlatTextBox5.Size = New System.Drawing.Size(270, 29)
+        Me.FlatTextBox5.TabIndex = 13
+        Me.FlatTextBox5.Text = "Generating MAC..."
+        Me.FlatTextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.FlatTextBox5.TextColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.FlatTextBox5.UseSystemPasswordChar = False
+        '
+        'FlatTextBox4
+        '
+        Me.FlatTextBox4.BackColor = System.Drawing.Color.Transparent
+        Me.FlatTextBox4.Location = New System.Drawing.Point(11, 191)
+        Me.FlatTextBox4.MaxLength = 32767
+        Me.FlatTextBox4.Multiline = False
+        Me.FlatTextBox4.Name = "FlatTextBox4"
+        Me.FlatTextBox4.ReadOnly = False
+        Me.FlatTextBox4.Size = New System.Drawing.Size(270, 29)
+        Me.FlatTextBox4.TabIndex = 12
+        Me.FlatTextBox4.Text = "Generating MB..."
+        Me.FlatTextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.FlatTextBox4.TextColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.FlatTextBox4.UseSystemPasswordChar = False
+        '
+        'FlatTextBox3
+        '
+        Me.FlatTextBox3.BackColor = System.Drawing.Color.Transparent
+        Me.FlatTextBox3.Location = New System.Drawing.Point(11, 151)
+        Me.FlatTextBox3.MaxLength = 32767
+        Me.FlatTextBox3.Multiline = False
+        Me.FlatTextBox3.Name = "FlatTextBox3"
+        Me.FlatTextBox3.ReadOnly = False
+        Me.FlatTextBox3.Size = New System.Drawing.Size(270, 29)
+        Me.FlatTextBox3.TabIndex = 11
+        Me.FlatTextBox3.Text = "Generating CPU..."
+        Me.FlatTextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.FlatTextBox3.TextColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.FlatTextBox3.UseSystemPasswordChar = False
+        '
+        'FlatTextBox2
+        '
+        Me.FlatTextBox2.BackColor = System.Drawing.Color.Transparent
+        Me.FlatTextBox2.Location = New System.Drawing.Point(11, 106)
+        Me.FlatTextBox2.MaxLength = 32767
+        Me.FlatTextBox2.Multiline = False
+        Me.FlatTextBox2.Name = "FlatTextBox2"
+        Me.FlatTextBox2.ReadOnly = False
+        Me.FlatTextBox2.Size = New System.Drawing.Size(270, 29)
+        Me.FlatTextBox2.TabIndex = 10
+        Me.FlatTextBox2.Text = "Generating HDD..."
+        Me.FlatTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.FlatTextBox2.TextColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.FlatTextBox2.UseSystemPasswordChar = False
+        '
+        'FlatLabel2
+        '
+        Me.FlatLabel2.AutoSize = True
+        Me.FlatLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatLabel2.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel2.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel2.Location = New System.Drawing.Point(226, 315)
+        Me.FlatLabel2.Name = "FlatLabel2"
+        Me.FlatLabel2.Size = New System.Drawing.Size(55, 13)
+        Me.FlatLabel2.TabIndex = 9
+        Me.FlatLabel2.Text = "DISCORD"
+        '
+        'FlatLabel1
+        '
+        Me.FlatLabel1.AutoSize = True
+        Me.FlatLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatLabel1.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.FlatLabel1.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel1.Location = New System.Drawing.Point(8, 315)
+        Me.FlatLabel1.Name = "FlatLabel1"
+        Me.FlatLabel1.Size = New System.Drawing.Size(55, 13)
+        Me.FlatLabel1.TabIndex = 8
+        Me.FlatLabel1.Text = "YOUTUBE"
+        '
+        'FlatTextBox1
+        '
+        Me.FlatTextBox1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatTextBox1.Location = New System.Drawing.Point(11, 59)
+        Me.FlatTextBox1.MaxLength = 32767
+        Me.FlatTextBox1.Multiline = False
+        Me.FlatTextBox1.Name = "FlatTextBox1"
+        Me.FlatTextBox1.ReadOnly = False
+        Me.FlatTextBox1.Size = New System.Drawing.Size(270, 29)
+        Me.FlatTextBox1.TabIndex = 7
+        Me.FlatTextBox1.Text = "Generating HWID..."
+        Me.FlatTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.FlatTextBox1.TextColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.FlatTextBox1.UseSystemPasswordChar = False
+        '
+        'FlatMini1
+        '
+        Me.FlatMini1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatMini1.BackColor = System.Drawing.Color.White
+        Me.FlatMini1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatMini1.Font = New System.Drawing.Font("Marlett", 12.0!)
+        Me.FlatMini1.Location = New System.Drawing.Point(250, 13)
+        Me.FlatMini1.Name = "FlatMini1"
+        Me.FlatMini1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatMini1.TabIndex = 6
+        Me.FlatMini1.Text = "FlatMini1"
+        Me.FlatMini1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatClose1
+        '
+        Me.FlatClose1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatClose1.BackColor = System.Drawing.Color.White
+        Me.FlatClose1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(168, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.FlatClose1.Font = New System.Drawing.Font("Marlett", 10.0!)
+        Me.FlatClose1.Location = New System.Drawing.Point(274, 13)
+        Me.FlatClose1.Name = "FlatClose1"
+        Me.FlatClose1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatClose1.TabIndex = 5
+        Me.FlatClose1.Text = "FlatClose1"
+        Me.FlatClose1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(295, 334)
+        Me.Controls.Add(Me.lblNetwork)
+        Me.Controls.Add(Me.FormSkin1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.RightToLeftLayout = True
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MONSTERMC DEVICE INFOS ID"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkin1.ResumeLayout(False)
+        Me.FormSkin1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblNetwork As Label
+    Friend WithEvents FormSkin1 As FormSkin
+    Friend WithEvents FlatTextBox1 As FlatTextBox
+    Friend WithEvents FlatMini1 As FlatMini
+    Friend WithEvents FlatClose1 As FlatClose
+    Friend WithEvents FlatLabel2 As FlatLabel
+    Friend WithEvents FlatLabel1 As FlatLabel
+    Friend WithEvents FlatTextBox4 As FlatTextBox
+    Friend WithEvents FlatTextBox3 As FlatTextBox
+    Friend WithEvents FlatTextBox2 As FlatTextBox
+    Friend WithEvents FlatTextBox6 As FlatTextBox
+    Friend WithEvents FlatTextBox5 As FlatTextBox
+    Friend WithEvents FlatLabel8 As FlatLabel
+    Friend WithEvents FlatLabel7 As FlatLabel
+    Friend WithEvents FlatLabel6 As FlatLabel
+    Friend WithEvents FlatLabel5 As FlatLabel
+    Friend WithEvents FlatLabel4 As FlatLabel
+    Friend WithEvents FlatLabel3 As FlatLabel
+End Class
